21e849ba4e01c7512629cd23cd3e0fb881e8f81d
JEURISS <JEURISS@NL-YA087687H.groupinfra.com>
2012-01-26 17:46:59 +0100

    - Linking to Framework Class Library Extension (isn't included in the commit!). - Refactored to start of MVVM.

---
+++ Laevo/Laevo/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+using Hardcodet.Wpf.TaskbarNotification;
+using Laevo.View.TrayIcon;
+
+
+namespace Laevo
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App
+	{
+		private TrayIconControl _trayIcon;
+
+
+		protected override void OnStartup( StartupEventArgs e )
+		{
+			base.OnStartup( e );
+
+			// TODO: Create Model.
+
+			// TODO: Create ViewModel.
+
+			// Create View.
+			_trayIcon = new TrayIconControl();
+		}
+	}
+}
