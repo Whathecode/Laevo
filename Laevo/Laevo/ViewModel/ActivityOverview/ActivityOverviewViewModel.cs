3460f6121652e60f5005cb551c68ddc140f526a4
Steven Jeuris <steven.jeuris@gmail.com>
2012-02-01 18:26:13 +0100

    Start of VirtualDesktopManager.

--- Laevo/Laevo/ViewModel/ActivityOverview/ActivityOverviewViewModel.cs
+++ Laevo/Laevo/ViewModel/ActivityOverview/ActivityOverviewViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using Laevo.ViewModel.ActivityOverview.Binding;
+using VirtualDesktopManager;
 using Whathecode.System.Windows.Aspects.ViewModel;
+using Whathecode.System.Windows.Input.CommandFactory.Attributes;
 
 
 namespace Laevo.ViewModel.ActivityOverview
@@ -7,5 +9,51 @@ namespace Laevo.ViewModel.ActivityOverview
 	[ViewModel( typeof( Binding.Properties ), typeof( Commands ) )]
 	class ActivityOverviewViewModel
 	{
+		public delegate void OpenedActivityEventHandler();
+		/// <summary>
+		///   Event which is triggered when an activity is opened.
+		/// </summary>
+		public event OpenedActivityEventHandler OpenedActivityEvent;
+
+		readonly DesktopManager _desktopManager = new DesktopManager();
+		readonly VirtualDesktop _initialDesktop;
+		readonly VirtualDesktop _desktop2;
+		readonly VirtualDesktop _desktop3;
+		readonly VirtualDesktop _desktop4;
+
+
+		public ActivityOverviewViewModel()
+		{
+			_initialDesktop = _desktopManager.CurrentDesktop;
+			_desktop2 = _desktopManager.CreateEmptyDesktop();
+			_desktop3 = _desktopManager.CreateEmptyDesktop();
+			_desktop4 = _desktopManager.CreateEmptyDesktop();		
+		}
+
+
+		[CommandExecute( Commands.OpenActivity )]
+		public void OpenActivity( string desktopId )
+		{
+			switch ( int.Parse( desktopId ) )
+			{
+				case 1:
+					_desktopManager.SwitchToDesktop( _initialDesktop );
+					break;
+				case 2:
+					_desktopManager.SwitchToDesktop( _desktop2 );
+					break;
+				case 3:
+					_desktopManager.SwitchToDesktop( _desktop3 );
+					break;
+				case 4:
+					_desktopManager.SwitchToDesktop( _desktop4 );
+					break;
+			}
+
+			if ( OpenedActivityEvent != null )
+			{
+				OpenedActivityEvent();
+			}
+		}
 	}
 }
---
+++ Laevo/VirtualDesktopManager/VirtualDesktop.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Whathecode.System.Windows.Interop;
+
+
+namespace VirtualDesktopManager
+{
+	/// <summary>
+	///   Represents a virtual desktop.
+	/// </summary>
+	/// <author>Steven Jeuris</author>
+	public class VirtualDesktop
+	{
+		internal ReadOnlyCollection<WindowInfo> Windows { get; private set; }
+
+
+		/// <summary>
+		///   Create an empty virtual desktop.
+		/// </summary>
+		internal VirtualDesktop() {}
+
+
+		/// <summary>
+		///   Create a virtual desktop which is initialized with a set of existing windows.
+		/// </summary>
+		/// <param name="initialWindows">The windows which should belong to the new virtual desktop.</param>
+		internal VirtualDesktop( IEnumerable<WindowInfo> initialWindows )
+		{
+			Windows = initialWindows.ToList().AsReadOnly();
+		}
+	}
+}
