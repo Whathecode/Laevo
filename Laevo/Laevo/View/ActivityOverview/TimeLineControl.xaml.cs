257a7bf5e42739e6674bc499efe249ac035318ab
Steven Jeuris <steven.jeuris@gmail.com>
2012-02-16 00:20:16 +0100

    Initial bindings to place elements on the correct place on the time line.

--- Laevo/Laevo/View/ActivityOverview/TimeLineControl.xaml.cs
+++ Laevo/Laevo/View/ActivityOverview/TimeLineControl.xaml.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 using Whathecode.System.Arithmetic.Range;
+using Whathecode.System.Extensions;
 using Whathecode.System.Windows.DependencyPropertyFactory.Aspects;
 using Whathecode.System.Windows.DependencyPropertyFactory.Attributes;
-using Whathecode.System.Extensions;
-using System.Linq;
 
 
 namespace Laevo.View.ActivityOverview
@@ -36,7 +37,7 @@ namespace Laevo.View.ActivityOverview
 		///   Collection of elements which are placed at a specific point, or timespan in time.
 		/// </summary>
 		[DependencyProperty( Properties.Children )]
-		public ObservableCollection<UIElement> Children { get; private set; }
+		public ObservableCollection<FrameworkElement> Children { get; private set; }
 
 
 		#region Attached properties
@@ -44,12 +45,13 @@ namespace Laevo.View.ActivityOverview
 		/// <summary>
 		///   Identifies the Occurance property which indicates where the element should be positioned on the time line.
 		/// </summary>
-		public static readonly DependencyProperty OccuranceProperty = DependencyProperty.RegisterAttached( "Occurance", typeof( DateTime ), typeof( TimeLineControl ) );
-		public static DateTime GetOccurance( UIElement element )
+		public static readonly DependencyProperty OccuranceProperty
+			= DependencyProperty.RegisterAttached( "Occurance", typeof( DateTime ), typeof( TimeLineControl ) );
+		public static DateTime GetOccurance( FrameworkElement element )
 		{
 			return (DateTime)element.GetValue( OccuranceProperty );
 		}
-		public static void SetOccurance( UIElement element, DateTime value )
+		public static void SetOccurance( FrameworkElement element, DateTime value )
 		{
 			element.SetValue( OccuranceProperty, value );
 		}
@@ -57,12 +59,13 @@ namespace Laevo.View.ActivityOverview
 		/// <summary>
 		///   Identifies the Offset property which indicates the vertical offset from the time line.
 		/// </summary>
-		public static readonly DependencyProperty OffsetProperty = DependencyProperty.RegisterAttached( "Offset", typeof( double ), typeof( TimeLineControl ) );
-		public static double GetOffset( UIElement element )
+		public static readonly DependencyProperty OffsetProperty
+			= DependencyProperty.RegisterAttached( "Offset", typeof( double ), typeof( TimeLineControl ) );
+		public static double GetOffset( FrameworkElement element )
 		{
 			return (double)element.GetValue( OffsetProperty );
 		}
-		public static void SetOffset( UIElement element, double value )
+		public static void SetOffset( FrameworkElement element, double value )
 		{
 			element.SetValue( OffsetProperty, value );
 		}
@@ -74,7 +77,7 @@ namespace Laevo.View.ActivityOverview
 		{
 			InitializeComponent();
 
-			Children = new ObservableCollection<UIElement>();
+			Children = new ObservableCollection<FrameworkElement>();
 			Children.CollectionChanged += OnChildrenChanged;
 		}
 
@@ -84,8 +87,25 @@ namespace Laevo.View.ActivityOverview
 			switch ( eventArgs.Action )
 			{
 				case NotifyCollectionChangedAction.Add:
-					// TODO: Bind to current viewport.
-					// TODO: Bind Canvas.Bottom to Offset.
+					eventArgs.NewItems.Cast<FrameworkElement>().ForEach( e =>
+					{
+						// Position horizontally.
+						MultiBinding positionBinding = new MultiBinding();
+						Binding widthBinding = new Binding( "ActualWidth" ) { Source = this };
+						positionBinding.Bindings.Add( widthBinding );
+						Binding elementWidthBinding = new Binding( "ActualWidth" ) { Source = e };
+						positionBinding.Bindings.Add( elementWidthBinding );
+						Binding viewportBinding = new Binding( "VisibleInterval" ) { Source = this };
+						positionBinding.Bindings.Add( viewportBinding );
+						Binding occuranceBinding = new Binding { Path = new PropertyPath( OccuranceProperty ), Source = e };
+						positionBinding.Bindings.Add( occuranceBinding );
+						positionBinding.Converter = new TimeLinePositionConverter();
+						e.SetBinding( Canvas.LeftProperty, positionBinding );
+
+						// Position vertically.
+						Binding bottomBinding = new Binding { Path = new PropertyPath( OffsetProperty ), Source = e };
+						e.SetBinding( Canvas.BottomProperty, bottomBinding );
+					} );
 					break;
 			}
 		}
