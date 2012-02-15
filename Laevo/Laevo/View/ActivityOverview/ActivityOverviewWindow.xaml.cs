d0e309ea0df60c50577f8c144cd1cf7cef859e95
jeuriss <jeuriss@NL-YA087687H.groupinfra.com>
2012-02-15 17:23:59 +0100

    Start of time line control.

--- Laevo/Laevo/View/ActivityOverview/ActivityOverviewWindow.xaml.cs
+++ Laevo/Laevo/View/ActivityOverview/ActivityOverviewWindow.xaml.cs
@@ -1,4 +1,9 @@
-﻿namespace Laevo.View.ActivityOverview
+﻿using System;
+using System.Windows.Controls;
+using Whathecode.System.Arithmetic.Range;
+
+
+namespace Laevo.View.ActivityOverview
 {
 	/// <summary>
 	/// Interaction logic for ActivityOverviewWindow.xaml
@@ -8,6 +13,15 @@
 		public ActivityOverviewWindow()
 		{
 			InitializeComponent();
+
+			DateTime now = DateTime.Now;
+			TimeLine.VisibleInterval = new Interval<DateTime>( now, now + TimeSpan.FromDays( 3 ) );
+			var day1 = new Button { Content = "Day 1", Width = 100, Height = 100 };
+			day1.SetValue( TimeLineControl.OccuranceProperty, now + TimeSpan.FromDays( 1 ) );
+			var day2 = new Button { Content = "Day 2", Width = 100, Height = 100 };
+			day2.SetValue( TimeLineControl.OccuranceProperty, now + TimeSpan.FromDays( 2 ) );
+			TimeLine.Children.Add( day1 );
+			TimeLine.Children.Add( day2 );
 		}
 	}
 }
--- Laevo/Laevo/ViewModel/ActivityOverview/Binding/Binding.cs
+++ Laevo/Laevo/ViewModel/ActivityOverview/Binding/Binding.cs
@@ -1,12 +1,6 @@
 ﻿namespace Laevo.ViewModel.ActivityOverview.Binding
 {
-	public enum Properties
-	{
-		Activity1,
-		Activity2,
-		Activity3,
-		Activity4
-	}
+	public enum Properties { }
 
 	public enum Commands { }
 }
