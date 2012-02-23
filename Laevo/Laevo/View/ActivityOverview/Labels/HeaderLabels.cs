8c18cdb3d987b6bbeb3ff663e74cce949c0113d4
jeuriss <jeuriss@NL-YA087687H.groupinfra.com>
2012-02-23 15:19:03 +0100

    First attempt at overriding binding for the first visible time label. (doesn't work so far)

--- Laevo/Laevo/View/ActivityOverview/Labels/HeaderLabels.cs
+++ Laevo/Laevo/View/ActivityOverview/Labels/HeaderLabels.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Controls;
+using System.Windows.Data;
 using System.Windows.Media;
 using Whathecode.System.Arithmetic.Range;
 using System.Windows;
+using Whathecode.System.Extensions;
 
 
 namespace Laevo.View.ActivityOverview.Labels
@@ -23,6 +25,17 @@ namespace Laevo.View.ActivityOverview.Labels
 		///   The smallest interval which is too large to fit the screen vertically. This is the most relevant 'depth'.
 		/// </summary>
 		RegularInterval _currentDepth;
+
+		/// <summary>
+		///   The time of the earliest label which is positioned.
+		/// </summary>
+		DateTime _earliestLabelTime;
+
+		/// <summary>
+		///   Cached data of the label which is identified as the earliest, used to restore it when it no longer is the earliest label.
+		/// </summary>
+		Tuple<TextBlock, Binding> _earliestLabelCached;
+
 		readonly Dictionary<TextBlock, RegularInterval> _matchLabelsToDepth = new Dictionary<TextBlock, RegularInterval>();
 
 
@@ -53,9 +66,14 @@ namespace Laevo.View.ActivityOverview.Labels
 			// Get the smallest interval which is currently the most relevant.
 			_currentDepth = _intervals.Reverse().FirstOrDefault( IsIntervalHigherThanScreen );
 
-			return _currentDepth != null
-				? _currentDepth.GetPositions( interval )
-				: new DateTime[] { };
+			if ( _currentDepth == null )
+			{
+				return new DateTime[] { };
+			}
+
+			DateTime[] positions = _currentDepth.GetPositions( interval ).ToArray();
+			_earliestLabelTime = positions[ 0 ];
+			return positions;
 		}
 
 		protected override TextBlock CreateNewLabel()
@@ -74,9 +92,20 @@ namespace Laevo.View.ActivityOverview.Labels
 			_matchLabelsToDepth[ label ] = _currentDepth;
 
 			var occurance = (DateTime)label.GetValue( TimeLineControl.OccuranceProperty );
+			label.Text = _currentDepth.Format( occurance );		
+			label.SetValue( TimeLineControl.OffsetProperty, TimeLine.ActualHeight - label.ActualHeight + VerticalLabelOffset );
 
-			label.Text = _currentDepth.Format( occurance );
-			label.SetValue( TimeLineControl.OffsetProperty, TimeLine.ActualHeight - label.ActualHeight + VerticalLabelOffset );	
+			// Override positioning of the earliest (first) label which is shown.
+			/*if ( occurance == _earliestLabelTime )
+			{
+				if ( _earliestLabelCached != null && _earliestLabelCached.Item2 != null )
+				{
+					_earliestLabelCached.Item1.SetBinding( Canvas.LeftProperty, _earliestLabelCached.Item2 );
+				}
+
+				_earliestLabelCached = new Tuple<TextBlock, Binding>( label, BindingOperations.GetBinding( label, Canvas.LeftProperty ) );
+				label.SetValue( Canvas.LeftProperty, HorizontalLabelOffset );
+			}*/
 		}
 	}
 }
