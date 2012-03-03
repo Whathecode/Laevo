0d293c167e3aac49534d99f30bebce6016774021
Steven <Steven@AIM-MSI-X2.lan>
2012-03-03 03:03:41 +0100

    Applied rotation to the time line.

---
+++ Laevo/Laevo/View/ActivityOverview/Converters/AspectRatioToGeometryConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media.Media3D;
+
+
+namespace Laevo.View.ActivityOverview.Converters
+{
+	public class AspectRatioToGeometryConverter : IMultiValueConverter
+	{
+		public object Convert( object[] values, Type targetType, object parameter, CultureInfo culture )
+		{
+			double ratio = (double)values[ 0 ] / (double)values[ 1 ];
+
+			// TODO: Calculate exactly how much bigger the time line should be to prevent excess rendering.
+			// See TimeLineWidthConverter as well.
+			var points = new[]
+			{
+				new Point3D( -ratio, 1, 0 ),	// Top left.
+				new Point3D( -ratio, -1, 0 ),	// Bottom left.							
+				new Point3D( ratio * 3, -1, 0 ),	// Bottom right.
+				new Point3D( ratio * 3, 1, 0 ),		// Top right.	
+			};
+
+			return new Point3DCollection( points );
+		}
+
+		public object[] ConvertBack( object value, Type[] targetTypes, object parameter, CultureInfo culture )
+		{
+			throw new NotSupportedException();
+		}
+	}
+}
---
+++ Laevo/Laevo/View/ActivityOverview/Converters/AspectRatioToTransformOffset.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+
+namespace Laevo.View.ActivityOverview.Converters
+{
+	public class AspectRatioToTransformOffset : IMultiValueConverter
+	{
+		public object Convert( object[] values, Type targetType, object parameter, CultureInfo culture )
+		{
+			double ratio = (double)values[ 0 ] / (double)values[ 1 ];
+			return -ratio;
+		}
+
+		public object[] ConvertBack( object value, Type[] targetTypes, object parameter, CultureInfo culture )
+		{
+			throw new NotSupportedException();
+		}
+	}
+}
