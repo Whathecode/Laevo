72e3312191246b37848a63fffe6f9ac3d3cf0218
Steven Jeuris <steven.jeuris@gmail.com>
2012-03-06 19:07:34 +0100

    Added clock to activity overview. (Forget to add new files)

---
+++ Laevo/Laevo/View/ActivityOverview/ClockControl.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Whathecode.System.Windows.DependencyPropertyFactory.Aspects;
+using Whathecode.System.Windows.DependencyPropertyFactory.Attributes;
+
+
+namespace Laevo.View.ActivityOverview
+{
+	/// <summary>
+	///   Interaction logic for ClockControl.xaml
+	///   TODO: Make format strings for date and time bindable?
+	///         While attempting this I ran into problems you can't bind to properties of converters.
+	///         A possible solution: http://www.codeproject.com/Articles/18678/Attaching-a-Virtual-Branch-to-the-Logical-Tree-in
+	/// </summary>
+	[WpfControl( typeof( Properties ) )]
+	public partial class ClockControl
+	{
+		[Flags]
+		public enum Properties
+		{
+			Time
+		}
+
+		/// <summary>
+		///   The currently visible interval.
+		/// </summary>
+		[DependencyProperty( Properties.Time )]
+		public DateTime Time { get; set; }
+
+
+		public ClockControl()
+		{
+			InitializeComponent();
+		}
+	}
+}
