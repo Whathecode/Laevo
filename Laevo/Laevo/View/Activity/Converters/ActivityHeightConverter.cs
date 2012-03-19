a0667f63d35af5fe402d52bf4d92a492ba3ed9d1
Steven <Steven@AIM-MSI-X2.home>
2012-03-19 15:18:01 +0100

    Activity height is now specified using a percentage of the available space.

---
+++ Laevo/Laevo/View/Activity/Converters/ActivityHeightConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+
+namespace Laevo.View.Activity.Converters
+{
+	public class ActivityHeightConverter : IMultiValueConverter
+	{
+		public object Convert( object[] values, Type targetType, object parameter, CultureInfo culture )
+		{
+			// TODO: Get these offsets from somewhere else instead.
+			const double topOffset = 90;
+			const double bottomOffset = 45;
+
+			double heightPercentage = (double)values[ 0 ];
+			double headerHeight = (double)values[ 1 ];
+			double availableHeight = (double)values[ 2 ] - topOffset - bottomOffset;
+
+			double size = (availableHeight * heightPercentage) - headerHeight;
+			return size < 0 ? 0 : size;
+		}
+
+		public object[] ConvertBack( object value, Type[] targetTypes, object parameter, CultureInfo culture )
+		{
+			throw new NotSupportedException();
+		}
+	}
+}
