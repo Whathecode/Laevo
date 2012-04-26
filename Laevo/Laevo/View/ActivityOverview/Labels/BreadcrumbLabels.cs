21f1357827c31b0b6157277c84c2663bef3af955
Steven Jeuris <steven.jeuris@gmail.com>
2012-04-26 03:04:36 +0200

    Further performance improvement by caching previous DateTime formattings.

--- Laevo/Laevo/View/ActivityOverview/Labels/BreadcrumbLabels.cs
+++ Laevo/Laevo/View/ActivityOverview/Labels/BreadcrumbLabels.cs
@@ -5,6 +5,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using Whathecode.System.Arithmetic.Range;
+using Whathecode.System.Extensions;
 using Whathecode.System.Linq;
 
 
@@ -15,10 +16,13 @@ namespace Laevo.View.ActivityOverview.Labels
 		const double HorizontalLabelOffset = 13.0;
 		const double VerticalLabelOffset = -60.0;
 
+		bool _currentDepthChanged;
+
 
 		public BreadcrumbLabels( TimeLineControl timeLine )
 			: base( timeLine )
 		{
+			CurrentDepthChanged += () => _currentDepthChanged = true;
 		}
 
 
@@ -34,25 +38,39 @@ namespace Laevo.View.ActivityOverview.Labels
 			};
 		}
 
+		readonly Dictionary<DateTime, string> _formattedDates = new Dictionary<DateTime, string>();
 		protected override DateTime[] GetTopLabelPositions( Interval<DateTime> interval )
 		{
+			if ( _currentDepthChanged )
+			{
+				// Different depths may use different formats.
+				_formattedDates.Clear();
+			}
+
 			IEnumerable<DateTime> flattened = Intervals
 				.TakeWhile( i => i != CurrentDepth )
-				.SelectMany( i => i.GetPositions( interval ) )
-				.OrderBy( d => d )
-				// For performance reasons, do the formatting once prior to calling Distinct().
-				.Select( d => new { Date = d, Formatted = Formatting[ CurrentDepth ]( d ) } )
-				.Distinct( d => d.Formatted )
-				.Select( d => d.Date );
+				.SelectMany( i => i.GetPositions( interval ) );
 
-			if ( flattened.Count() >= 2 )
+			// Update the list of formatted dates.
+			var toRemove = _formattedDates.Keys.Where( d => !flattened.Contains( d ) ).ToArray();
+			toRemove.ForEach( d => _formattedDates.Remove( d ) );
+			var toAdd = flattened.Where( d => !_formattedDates.ContainsKey( d ) ).ToArray();
+			toAdd.ForEach( d => _formattedDates[ d ] = Formatting[ CurrentDepth ]( d ) );
+
+			// Filter out the dates which are represented similarly.
+			var filtered = _formattedDates
+				.OrderBy( p => p.Key )
+				.Distinct( p => p.Value )
+				.Select( p => p.Key );
+
+			if ( filtered.Count() >= 2 )
 			{
 				// Only keep one position which lies in front of the time line.
-				return flattened
-					.Zip( flattened.Skip( 1 ), Tuple.Create )
+				return filtered
+					.Zip( filtered.Skip( 1 ), Tuple.Create )
 					.SkipWhile( t => t.Item2 <= TimeLine.VisibleInterval.Start )
 					.Select( t => t.Item1 )
-					.Concat( new [] { flattened.Last() } ) // Don't forget the last! It was dropped when zipping with Skip().
+					.Concat( new [] { filtered.Last() } ) // Don't forget the last! It was dropped when zipping with Skip().
 					.ToArray();
 			}
 
