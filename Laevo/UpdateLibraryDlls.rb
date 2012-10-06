6aefe7bcf6524b0271cbee784f7244e857651280
Steven Jeuris <steven.jeuris@gmail.com>
2012-10-06 19:17:51 +0200

    Set up a post build script for the Release build which updates the DLLs in the Libraries folder.

---
+++ Laevo/UpdateLibraryDlls.rb
@@ -0,0 +1,35 @@
+require 'xml'
+require 'fileutils'
+
+Dir.chdir(ARGV[0].to_s())
+parser = XML::Parser.file('ProjectReferences.txt')
+doc = parser.parse
+namespace = 'msbuild:http://schemas.microsoft.com/developer/msbuild/2003'
+
+# Change working directory to the Laevo project folder, since the paths might be relative paths from there.
+Dir.chdir('Laevo')
+
+fcl = doc.find_first('//msbuild:Framework-Class-Library-Extension', namespace).first.to_s()
+vdm = doc.find_first('//msbuild:Virtual-Desktop-Manager-API', namespace).first.to_s()
+
+# Copy Framework Class Library Extension DLLs.
+fcl_library = '..\\Libraries\\Framework Class Library Extension\\'
+fcl_dlls = [
+	'Whathecode.System',
+	'Whathecode.System.Aspects',
+	'Whathecode.PresentationFramework',
+	'Whathecode.PresentationFramework.Aspects'
+	]
+fcl_dlls.each do |d|
+	FileUtils.cp(
+		fcl + '\\' + d + '\\bin\\x86\\Release\\' + d + '.dll',
+		fcl_library + d + '.dll')
+end
+
+# Copy Virtual Desktop Manager API DLLs.
+vdm_dlls = [ 'Whathecode.System', 'Whathecode.VirtualDesktopManagerAPI' ]
+vdm_dlls.each do |d|
+	FileUtils.cp(
+		vdm + '\\bin\\x86\\Release\\' + d + '.dll',
+		'..\\Libraries\\VirtualDesktopManagerAPI\\' + d + '.dll')
+end
