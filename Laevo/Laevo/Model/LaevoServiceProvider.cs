c4f63bb77fe5200f61482164a1a18f574165ce33
Steven Jeuris <steven.jeuris@gmail.com>
2013-08-22 11:58:56 +0200

    Added missing LaevoServiceProvider, probably forgot this during commit #180. :O

---
+++ Laevo/Laevo/Model/LaevoServiceProvider.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.Composition;
+using ABC.Applications;
+
+
+namespace Laevo.Model
+{
+	[Export( typeof( ServiceProvider ) )]
+	public class LaevoServiceProvider : ServiceProvider
+	{
+		public LaevoServiceProvider()
+		{
+			ForceOpenInNewWindow = true;
+		}
+	}
+}
