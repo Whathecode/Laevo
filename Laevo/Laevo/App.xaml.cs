9fd3675170c59367e080db62af5bae084b0449ab
Steven Jeuris <Steven Jeuris@Steven-Asus.itu.dk>
2013-12-04 16:56:57 +0100

    Added main controller which acts as an entry point for Laevo and sets up the MVVM components. Also fixed the post build event which didn't work properly for some paths.

--- Laevo/Laevo/App.xaml.cs
+++ Laevo/Laevo/App.xaml.cs
@@ -5,8 +5,6 @@ using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Windows;
-using Laevo.View.Main;
-using Laevo.ViewModel.Main;
 using Whathecode.System.Aspects;
 
 
@@ -18,10 +16,7 @@ namespace Laevo
 	/// </summary>
 	public partial class App
 	{
-		Model.Laevo _model;
-		MainViewModel _viewModel;
-		TrayIconControl _trayIcon;
-
+		LaevoController _controller;
 
 		protected override void OnStartup( StartupEventArgs e )
 		{
@@ -46,20 +41,13 @@ namespace Laevo
 			DispatcherUnhandledException += ( s, a )
 				=> File.AppendAllText( Path.Combine( Model.Laevo.ProgramLocalDataFolder, "log.txt" ), a.Exception.ToString() + Environment.NewLine );
 
-			// Create Model.
-			_model = new Model.Laevo();
-
-			// Create ViewModel.
-			_viewModel = new MainViewModel( _model );
-
-			// Create View.
-			_trayIcon = new TrayIconControl( _viewModel ) { DataContext = _viewModel };
+			// Initiate the controller which sets up the MVVM classes.
+			_controller = new LaevoController();
 		}
 
 		protected override void OnExit( ExitEventArgs e )
 		{
-			_viewModel.Dispose();
-			_trayIcon.Dispose();
+			_controller.Dispose();
 		}
 	}
 }
---
+++ Laevo/Laevo/LaevoController.cs
@@ -0,0 +1,40 @@
+﻿using Laevo.View.Main;
+using Laevo.ViewModel.Main;
+using Whathecode.System;
+
+
+namespace Laevo
+{
+	/// <summary>
+	///   The main access point of Laevo, which acts as a controller hooking the different components of the Model/View/ViewModel together.
+	/// </summary>
+	class LaevoController : AbstractDisposable
+	{
+		readonly MainViewModel _viewModel;
+		readonly TrayIconControl _trayIcon;
+
+
+		public LaevoController()
+		{
+			// Create Model.
+			var model = new Model.Laevo();
+
+			// Create ViewModel.
+			_viewModel = new MainViewModel( model );
+
+			// Create View.
+			_trayIcon = new TrayIconControl( _viewModel ) { DataContext = _viewModel };
+		}
+
+		protected override void FreeManagedResources()
+		{
+			// Nothing to do.
+		}
+
+		protected override void FreeUnmanagedResources()
+		{
+			_viewModel.Dispose();
+			_trayIcon.Dispose();
+		}
+	}
+}
