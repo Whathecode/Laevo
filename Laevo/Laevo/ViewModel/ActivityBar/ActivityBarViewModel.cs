78f8870d00967a92062a0825eca0a770dc49f0e5
domi <domi@localhost>
2014-01-17 17:20:56 +0100

    Refactored activity switcher, added label of currently selected activity. (there is small problem with bar hiding )

--- Laevo/Laevo/ViewModel/ActivityBar/ActivityBarViewModel.cs
+++ Laevo/Laevo/ViewModel/ActivityBar/ActivityBarViewModel.cs
@@ -1,10 +1,8 @@
 ﻿using System.Collections.ObjectModel;
-using System.Collections.Specialized;
 using Laevo.ViewModel.Activity;
 using Laevo.ViewModel.ActivityBar.Binding;
 using Whathecode.System.ComponentModel.NotifyPropertyFactory.Attributes;
 using Whathecode.System.Windows.Aspects.ViewModel;
-using Whathecode.System.Windows.Input.CommandFactory.Attributes;
 
 
 namespace Laevo.ViewModel.ActivityBar
@@ -12,6 +10,9 @@ namespace Laevo.ViewModel.ActivityBar
 	[ViewModel( typeof( Binding.Properties ), typeof( Commands ) )]
 	class ActivityBarViewModel : AbstractViewModel
 	{
+		int _selectionIndex = 1;
+		public View.ActivityBar.ActivityBar ActivityBar { get; set; }
+
 		/// <summary>
 		/// Home activity.
 		/// </summary>
@@ -30,9 +31,50 @@ namespace Laevo.ViewModel.ActivityBar
 		[NotifyProperty( Binding.Properties.CurrentActivity )]
 		public ActivityViewModel CurrentActivity { get; set; }
 
+		/// <summary>
+		/// Current activity.
+		/// </summary>
+		[NotifyProperty( Binding.Properties.SelectedActivity )]
+		public ActivityViewModel SelectedActivity { get; set; }
+
 		public ActivityBarViewModel()
 		{
 			OpenPlusCurrentActivities = new ObservableCollection<ActivityViewModel>();
+		}
+
+		/// <summary>
+		/// Selects next activity.
+		/// </summary>
+		internal void SelectNextActivity()
+		{
+			if ( OpenPlusCurrentActivities.Count > 1 )
+			{
+				// Come back on the beginning when selection index is outside of open plus active activities collection.
+				if ( _selectionIndex == OpenPlusCurrentActivities.Count )
+				{
+					_selectionIndex = 0;
+				}
+
+				SelectedActivity = OpenPlusCurrentActivities[ _selectionIndex ];
+				ActivityBar.SelectNextActivity( _selectionIndex );
+
+				_selectionIndex++;
+			}
+		}
+
+		/// <summary>
+		/// Activates selected activity.
+		/// </summary>
+		internal void ActivateSelectedActivity()
+		{
+			if ( SelectedActivity != null )
+			{
+				SelectedActivity.ActivateActivity( false );
+				SelectedActivity = null;
+
+				ActivityBar.CloseAndPassFocus();
+			}
+			_selectionIndex = 1;
 		}
 
 		protected override void FreeUnmanagedResources()
