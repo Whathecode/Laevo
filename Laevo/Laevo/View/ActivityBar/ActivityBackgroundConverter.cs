8da34f24f07d71bd4d808446feb631ab2953c4ff
domi <domi@localhost>
2014-02-20 16:57:16 +0100

    Changed activity switcher highlight color to be consistent with the rest of controls.

--- Laevo/Laevo/View/ActivityBar/ActivityBackgroundConverter.cs
+++ Laevo/Laevo/View/ActivityBar/ActivityBackgroundConverter.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Windows;
 using System.Windows.Media;
 using Laevo.ViewModel.Activity;
 using Whathecode.System.Windows.Data;
@@ -14,14 +16,21 @@ namespace Laevo.View.ActivityBar
 			var color = (Color)values[ 1 ];
 			var selectedActivity = (ActivityViewModel)values[ 2 ];
 
+			var gradientsStops = new List<GradientStop>
+			{
+				// ReSharper disable PossibleNullReferenceException
+				new GradientStop( (Color)ColorConverter.ConvertFromString( "#FFE3F4FC" ), 0 ),
+				new GradientStop( (Color)ColorConverter.ConvertFromString( "#FFD8EFFC" ), 0.38 ),
+				new GradientStop( (Color)ColorConverter.ConvertFromString( "#FFBEE6FD" ), 0.38 ),
+				new GradientStop( (Color)ColorConverter.ConvertFromString( "#FFA6D9F4" ), 1 )
+			};
+			var highlightBrush = new LinearGradientBrush(new GradientStopCollection(gradientsStops), new Point(0, 0), new Point(0, 1));
+
 			if ( activity == selectedActivity )
 			{
-				return new LinearGradientBrush( Colors.White, Colors.Yellow, 90 );
+				return highlightBrush; 
 			}
-			else
-			{
-				return new SolidColorBrush( color );
-			}
+			return new SolidColorBrush( color );
 		}
 
 		public override object[] ConvertBack( Brush value )
