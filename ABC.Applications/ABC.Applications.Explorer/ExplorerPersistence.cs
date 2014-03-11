d0c8c6005e47b9306ca279d9dd3f0640e8d54a71
Steven Jeuris <steven.jeuris@gmail.com>
2014-03-11 18:19:18 +0100

    Explorer persistence now can restore virtual locations as well.

--- ABC.Applications/ABC.Applications.Explorer/ExplorerPersistence.cs
+++ ABC.Applications/ABC.Applications.Explorer/ExplorerPersistence.cs
@@ -1,12 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using ABC.Applications.Persistence;
-using Microsoft.Win32;
 using SHDocVw;
-using Whathecode.System.Diagnostics;
+using Whathecode.Interop;
 using Whathecode.System.Extensions;
 
 
@@ -16,53 +15,15 @@ namespace ABC.Applications.Explorer
 	{
 		public string LocationName { get; set; }
 		public string LocationUrl { get; set; }
+		public byte[] Pidl { get; set; }
 	}
 
 
 	[Export( typeof( AbstractApplicationPersistence ) )]
 	public class ExplorerPersistence : AbstractApplicationPersistence
 	{
-		// TODO: The _shellFolderNames collection is currently filled up in the constructor, but is only a hackish attempt at finding out which windows is open when LocationURL is not specified.
-		//       Can we somehow find out the actual thing visible in the explorer window? http://stackoverflow.com/q/22284718/590790
-		readonly Dictionary<string, string> _shellFolderNames = new Dictionary<string, string>(); 
-
-
 		public ExplorerPersistence()
-			: base( "explorer" )
-		{
-			// First try to find CLSIDs, which don't always appear to work but seem to be more complete.
-			using ( RegistryKey clsids = Registry.ClassesRoot.OpenSubKey( "CLSID" ) )
-			{
-				foreach ( string clsid in clsids.GetSubKeyNames().Where( clsid => clsids.OpenSubKey( clsid + "\\ShellFolder" ) != null ) )
-				{
-					using ( RegistryKey shellFolder = clsids.OpenSubKey( clsid ) )
-					{
-						// Try to use the localized name, otherwise the default name.
-						string name = shellFolder.LoadMuiStringValue( "LocalizedString" ) ?? (string)shellFolder.GetValue( "" );
-						if ( name != null )
-						{
-							_shellFolderNames[ name ] = "::" + clsid;
-						}
-					}
-				}
-			}
-
-			// Alternatively, load shell description names which can be opened using the "shell:Name" parameter.
-			using ( RegistryKey clsids = Registry.LocalMachine.OpenSubKey( @"SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FolderDescriptions" ) )
-			{
-				foreach ( string clsid in clsids.GetSubKeyNames() )
-				{
-					using ( RegistryKey folderDescription = clsids.OpenSubKey( clsid ) )
-					{
-						string localized = folderDescription.LoadMuiStringValue( "LocalizedName" );
-						if ( localized != null )
-						{
-							_shellFolderNames[ localized ] = "shell:" + (string)folderDescription.GetValue( "Name" );
-						}
-					}
-				}
-			}
-		}
+			: base( "explorer" ) {}
 
 
 		public override object Suspend( SuspendInformation toSuspend )
@@ -79,7 +40,8 @@ namespace ABC.Applications.Explorer
 			var persistedData = new ExplorerLocation
 			{
 				LocationName = window.LocationName,
-				LocationUrl = window.LocationURL
+				LocationUrl = window.LocationURL,
+				Pidl = window.GetPidl()
 			};
 
 			window.Quit();
@@ -91,16 +53,21 @@ namespace ABC.Applications.Explorer
 		{
 			var location = (ExplorerLocation)persistedData;
 
-			// Start out assuming explorer points to a simple path.
-			string openFolder = location.LocationUrl;
-
-			// Check whether the open folder is a shell folder.
-			if ( String.IsNullOrEmpty( openFolder ) && _shellFolderNames.ContainsKey( location.LocationName ) )
+			// Copy the PIDL to unmanaged memory.
+			IntPtr pidl = Marshal.AllocHGlobal( location.Pidl.Length );
+			Marshal.Copy( location.Pidl, 0, pidl, location.Pidl.Length );
+			
+			// Open an explorer window with the passed PIDL.
+			var executeInfo = new Shell32.ShellExecuteInfo
 			{
-				openFolder = _shellFolderNames[ location.LocationName ];
-			}
-
-			ProcessHelper.SetUp( applicationPath, openFolder ).Run();
+				Type = Shell32.ShellExecuteInfoType.Pidl,
+				Pidl = pidl,
+				Show = User32.WindowState.ShowNoActivate,
+				StructSize = (uint)Marshal.SizeOf( typeof( Shell32.ShellExecuteInfo ) )
+			};
+			Shell32.ShellExecuteEx( ref executeInfo );
+			
+			Marshal.Release( pidl );
 		}
 
 		public override Type GetPersistedDataType()
