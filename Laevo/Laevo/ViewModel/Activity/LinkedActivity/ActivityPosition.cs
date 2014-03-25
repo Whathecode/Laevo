0f8b080a5e65e0dc1f401b742de6c2d949d3967e
Steven Jeuris <Steven Jeuris@Steven-Asus.itu.dk>
2014-03-25 19:11:33 +0100

    Refactoring of ActivityPosition to use flags indicating whether the linked activity shows the start/end of an activity or not.

--- Laevo/Laevo/ViewModel/Activity/LinkedActivity/ActivityPosition.cs
+++ Laevo/Laevo/ViewModel/Activity/LinkedActivity/ActivityPosition.cs
@@ -1,10 +1,25 @@
-﻿namespace Laevo.ViewModel.Activity.LinkedActivity
+﻿using System;
+
+
+namespace Laevo.ViewModel.Activity.LinkedActivity
 {
+	/// <summary>
+	///   Determines which part of the activity a <see cref="LinkedActivityViewModel" /> shows.
+	/// </summary>
+	[Flags]
 	public enum ActivityPosition
 	{
+		/// <summary>
+		///   The view model does not show the start, nor the end of the activity.
+		/// </summary>
+		None,
+		/// <summary>
+		///   The view model shows the start of the activity. This is the first time the activity was opened.
+		/// </summary>
 		Start,
-		Middle,
-		End,
-		None
+		/// <summary>
+		///   The view model shows the end of the activity. This is the last time the activity was or is open.
+		/// </summary>
+		End
 	};
 }
