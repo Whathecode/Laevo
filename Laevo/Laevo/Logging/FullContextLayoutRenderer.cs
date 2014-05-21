92992c1b9f469d7e67773e31496132f81cedf4c0
Steven Jeuris <steven.jeuris@gmail.com>
2014-05-21 18:57:15 +0200

    Allow passing properties to log items. Added logging for Laevo class.

---
+++ Laevo/Laevo/Logging/FullContextLayoutRenderer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using NLog;
+using NLog.LayoutRenderers;
+
+
+namespace Laevo.Logging
+{
+	[LayoutRenderer( "fullcontext" )]
+	class FullContextLayoutRenderer : LayoutRenderer
+	{
+		protected override void Append( StringBuilder builder, LogEventInfo logEvent )
+		{
+			int count = logEvent.Properties.Count;
+			if ( count == 0 )
+			{
+				return;
+			}
+
+			string context = "[ " + String.Join( ", ", logEvent.Properties.Select( kv => kv.Key + " = " + kv.Value ) ) + " ]";
+			builder.Append( context);
+		}
+	}
+}
---
+++ Laevo/Laevo/Logging/LogData.cs
@@ -0,0 +1,16 @@
+﻿namespace Laevo.Logging
+{
+	class LogData
+	{
+		public string Key { get; private set; }
+
+		public object Value { get; private set; }
+
+
+		public LogData( string key, object value )
+		{
+			Key = key;
+			Value = value;
+		}
+	}
+}
