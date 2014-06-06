65add8090b71b3dfd00d067ad2675ecaa2afd7e8
Dominik Grondziowski <5485317+dominkomin@users.noreply.github.com>
2014-06-06 15:55:36 +0200

    Persistance with backup on application crash.

--- Laevo/Laevo/Data/Common/PersistanceHelper.cs
+++ Laevo/Laevo/Data/Common/PersistanceHelper.cs
@@ -1,19 +1,25 @@
 ﻿using System;
 using System.IO;
 using System.Runtime.Serialization;
+using System.Windows;
+using NLog;
 
 
 namespace Laevo.Data.Common
 {
 	class PersistanceHelper
 	{
+		static readonly Logger Log = LogManager.GetCurrentClassLogger();
+		
+		public const string BackupExtention = ".backup";
+
 		public static void Persist( string file, DataContractSerializer serializer, object toSerialize )
 		{
 			// Make a backup first, so if serialization fails, no data is lost.
-			string backupFile = file + ".backup";
+			string backupFile = LaevoController.BackupFolder + file.Substring( file.LastIndexOf( '\\' ) ) + BackupExtention;
 			if ( File.Exists( file ) )
 			{
-				File.Copy( file, backupFile );
+				File.Copy( file, backupFile, true );
 			}
 
 			// Serialize the data.
@@ -24,20 +30,55 @@ namespace Laevo.Data.Common
 					serializer.WriteObject( fileStream, toSerialize );
 				}
 			}
-			catch ( Exception e )
+			catch ( Exception exception )
 			{
 				if ( File.Exists( backupFile ) )
 				{
 					File.Delete( file );
 					File.Move( backupFile, file );
 				}
-				throw new PersistenceException( "Serialization of data to file \"" + file + "\" failed. Recent data will be lost.", e );
+				Log.ErrorException( "Serialization failed.", exception );
+				throw new PersistenceException( "Serialization of data to file \"" + file + "\" failed. Recent data will be lost.", exception );
 			}
+		}
 
-			// Remove temporary backup file.
-			if ( File.Exists( backupFile ) )
+		public static object ReadData( string filePath, DataContractSerializer serializer, Type returnType )
+		{
+			try
 			{
-				File.Delete( backupFile );
+				return Deserialize( filePath, serializer );
+			}
+			catch ( Exception exception)
+			{
+				Log.ErrorException( "Deserialization failed.", exception );
+				string backupPath = LaevoController.BackupFolder + filePath.Substring( filePath.LastIndexOf( '\\' ) ) + PersistanceHelper.BackupExtention;
+				if ( File.Exists( backupPath ) )
+				{
+					try
+					{
+						return Deserialize( backupPath, serializer );
+					}
+					catch ( Exception backupException )
+					{
+						Log.ErrorException( "Deserialization from backup failed.", backupException );
+						return EmptyDeserialization( returnType, filePath );
+					}
+				}
+			}
+			return EmptyDeserialization( returnType, filePath );
+		}
+
+		static object EmptyDeserialization( Type returnType, string filePath )
+		{
+			MessageBox.Show( "Deserialization of the file " + filePath + " failed. Program will start without previous history.", "Laevo", MessageBoxButton.OK );
+			return Activator.CreateInstance( returnType );
+		}
+
+		static object Deserialize( string filePath, DataContractSerializer serializer )
+		{
+			using ( var fileStream = new FileStream( filePath, FileMode.Open ) )
+			{
+				return serializer.ReadObject( fileStream );
 			}
 		}
 	}
