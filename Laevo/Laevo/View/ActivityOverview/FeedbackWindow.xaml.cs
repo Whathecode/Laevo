2438d6e6c991f8f58d9593640357c890086c3208
Dominik Grondziowski <5485317+dominkomin@users.noreply.github.com>
2014-06-27 15:03:59 +0200

    Make a question feature added.

--- Laevo/Laevo/View/ActivityOverview/FeedbackWindow.xaml.cs
+++ Laevo/Laevo/View/ActivityOverview/FeedbackWindow.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System.Windows;
+﻿using System.Text.RegularExpressions;
+using System.Windows;
 using System.Windows.Controls;
 using Laevo.Logging;
+using Laevo.ViewModel.ActivityOverview;
 using NLog;
+using Xceed.Wpf.Toolkit;
 
 
 namespace Laevo.View.ActivityOverview
@@ -16,12 +19,18 @@ namespace Laevo.View.ActivityOverview
 		public FeedbackWindow()
 		{
 			InitializeComponent();
+			DataContextChanged += ( sender, args ) =>
+			{
+				var dataContext = (FeedbackViewModel)DataContext;
+				SendButton.IsEnabled = dataContext.FeedbackType != FeedbackType.Question;
+			};
 		}
 
 		void OnSendButtonClicked( object sender, RoutedEventArgs e )
 		{
-			var button = (Button)sender;
-			var logDatas = new[] { new LogData( "Feedback text", FeedbackTextBox.Text ), new LogData( "Email", EmailTextBox.Text ), new LogData( "Type", button.Content ) };
+			var dataContext = (FeedbackViewModel)DataContext;
+			var logDatas = new[]
+			{ new LogData( "Feedback text", FeedbackTextBox.Text ), new LogData( "Email", EmailTextBox.Text ), new LogData( "Type", dataContext.FeedbackType.ToString() ) };
 			Log.InfoWithData( "Feedback sent.", logDatas );
 			Hide();
 		}
@@ -29,6 +38,25 @@ namespace Laevo.View.ActivityOverview
 		void OnCancelButtonClicked( object sender, RoutedEventArgs e )
 		{
 			Hide();
+		}
+
+		void EmailChanged( object sender, TextChangedEventArgs e )
+		{
+			var dataContext = (FeedbackViewModel)DataContext;
+			if ( dataContext.FeedbackType != FeedbackType.Question )
+			{
+				return;
+			}
+			var textBox = (WatermarkTextBox)sender;
+			if ( textBox.Text.Length > 0 )
+			{
+				SendButton.IsEnabled = EmailIsValid( textBox.Text );
+			}
+		}
+
+		bool EmailIsValid( string emailaddress )
+		{
+			return Regex.IsMatch( emailaddress, @"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$" );
 		}
 	}
 }
--- Laevo/Laevo/ViewModel/ActivityOverview/FeedbackViewModel.cs
+++ Laevo/Laevo/ViewModel/ActivityOverview/FeedbackViewModel.cs
@@ -1,17 +1,23 @@
-﻿
-
-using System.Windows.Media;
+﻿using System.Windows.Media;
 
 
 namespace Laevo.ViewModel.ActivityOverview
 {
+	enum FeedbackType
+	{
+		Smile,
+		Frown,
+		Question
+	};
+
 	class FeedbackViewModel
 	{
-
+		public FeedbackType FeedbackType { get; set; }
 		public Color Color { get; set; }
 		public string SendText { get; set; }
 		public string FeedbackText { get; set; }
 		public string FeedbackTextHint { get; set; }
+		public string EmailTextHint { get; set; }
 		public ImageSource Image { get; set; }
 	}
 }
