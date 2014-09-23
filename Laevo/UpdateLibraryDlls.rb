cbd4c29133f54116f56be7470e7d0ceb668b2f07
Dominik Grondziowski <5485317+dominkomin@users.noreply.github.com>
2014-09-23 21:29:19 +0200

    Leavo customization to work with the newest ABC and WhatTheCode Frameworks.

--- Laevo/UpdateLibraryDlls.rb
+++ Laevo/UpdateLibraryDlls.rb
@@ -1,24 +1,23 @@
-require 'xml'
 require 'fileutils'
+require 'nokogiri'
 
+# Change working directory to the Laevo solution folder.
 Dir.chdir(ARGV[0].to_s())
-parser = XML::Parser.file('ProjectReferences.txt')
-doc = parser.parse
-namespace = 'msbuild:http://schemas.microsoft.com/developer/msbuild/2003'
 
-# Change working directory to the Laevo project folder, since the paths might be relative paths from there.
-Dir.chdir('Laevo')
-
-fcl = doc.find_first('//msbuild:Framework-Class-Library-Extension', namespace).first.to_s()
-abc = doc.find_first('//msbuild:ABC-Toolkit', namespace).first.to_s()
+# Get ABC and FCL project paths from ProjectReferences.txt.
+doc = Nokogiri::XML(open("..\\ProjectReferences.txt"))
+abc = doc.xpath("//nameSpace:PropertyGroup/nameSpace:ABC-Toolkit", {"nameSpace" => "http://schemas.microsoft.com/developer/msbuild/2003"}).text
+fcl = doc.xpath("//nameSpace:PropertyGroup/nameSpace:Framework-Class-Library-Extension", {"nameSpace" => "http://schemas.microsoft.com/developer/msbuild/2003"}).text
 
 # Copy Framework Class Library Extension DLLs.
 fcl_library = '..\\Libraries\\Framework Class Library Extension\\'
 fcl_dlls = [
-	'Whathecode.System',
-	'Whathecode.System.Aspects',
+	'Whathecode.Interop',
+	'Whathecode.Microsoft',
 	'Whathecode.PresentationFramework',
-	'Whathecode.PresentationFramework.Aspects'
+	'Whathecode.PresentationFramework.Aspects',
+	'Whathecode.System',
+	'Whathecode.System.Aspects'
 	]
 fcl_dlls.each do |d|
 	FileUtils.cp(
@@ -29,10 +28,11 @@ end
 # Copy ABC Toolkit DLLs.
 abc_toolkit = '..\\Libraries\\ABC Toolkit\\'
 abc_dlls = [
-	'ABC.Windows',
-	'ABC.PInvoke',
+	'ABC.Applications',
+	'ABC.Common',
 	'ABC.Interruptions',
-	'ABC.Applications'
+	'ABC.PInvoke',
+	'ABC.Windows'
 	]
 abc_dlls.each do |d|
 	FileUtils.cp(
