9e0eef31be1d673dfd0387b40869c5c2a7f6c879
Steven Jeuris <steven.jeuris@gmail.com>
2014-10-30 16:20:51 +0100

    No longer ignoring "ProjectReferences.txt" and "Laevo With Project References.sln" since they can now be used for a default side-by-side configuration.

--- Laevo/UpdateLibraryDlls.rb
+++ Laevo/UpdateLibraryDlls.rb
@@ -1,16 +1,16 @@
 require 'fileutils'
 require 'nokogiri'
 
-# Change working directory to the Laevo solution folder.
+# Change working directory to the Laevo project folder.
 Dir.chdir(ARGV[0].to_s())
 
 # Get ABC and FCL project paths from ProjectReferences.txt.
-doc = Nokogiri::XML(open("ProjectReferences.txt"))
+doc = Nokogiri::XML(open("..\\..\\ProjectReferences.txt"))
 abc = doc.xpath("//nameSpace:PropertyGroup/nameSpace:ABC-Toolkit", {"nameSpace" => "http://schemas.microsoft.com/developer/msbuild/2003"}).text
 fcl = doc.xpath("//nameSpace:PropertyGroup/nameSpace:Framework-Class-Library-Extension", {"nameSpace" => "http://schemas.microsoft.com/developer/msbuild/2003"}).text
 
 # Copy Framework Class Library Extension DLLs.
-fcl_library = '..\\Libraries\\Framework Class Library Extension\\'
+fcl_library = '..\\..\\Libraries\\Framework Class Library Extension\\'
 fcl_dlls = [
 	'Whathecode.Interop',
 	'Whathecode.Microsoft',
@@ -26,7 +26,7 @@ fcl_dlls.each do |d|
 end
 
 # Copy ABC Toolkit DLLs.
-abc_toolkit = '..\\Libraries\\ABC Toolkit\\'
+abc_toolkit = '..\\..\\Libraries\\ABC Toolkit\\'
 abc_dlls = [
 	'ABC.Applications',
 	'ABC.Common',
@@ -37,5 +37,14 @@ abc_dlls = [
 abc_dlls.each do |d|
 	FileUtils.cp(
 		abc + '\\' + d + '\\bin\\Release\\' + d + '.dll',
+		abc_toolkit + d + '.dll')
+end
+abc_fcl_dlls = [
+	'Whathecode.Interop',
+	'Whathecode.System'
+	]
+abc_fcl_dlls.each do |d|
+	FileUtils.cp(
+		fcl + '\\' + d + '\\bin\\Release\\' + d + '.dll',
 		abc_toolkit + d + '.dll')
 end
