78a62b5f37a19e498f266ae6c66ae5ed784ad151
domi <domi@localhost>
2014-11-25 13:58:26 +0100

    Handle unresponsive windows by adding more advanced pop-up window. Now user can decide which windows should be ignored or kept.

---
+++ Laevo/Laevo/ViewModel/Main/Unresponsive/UnresponsiveViewModel.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using ABC.Windows.Desktop;
+using Laevo.ViewModel.Main.Unresponsive.Binding;
+using Whathecode.System.ComponentModel.NotifyPropertyFactory.Attributes;
+using Whathecode.System.Extensions;
+using Whathecode.System.Windows.Aspects.ViewModel;
+using Whathecode.System.Windows.Input.CommandFactory.Attributes;
+
+
+namespace Laevo.ViewModel.Main.Unresponsive
+{
+	[ViewModel( typeof( Binding.Properties ), typeof( Commands ) )]
+	class UnresponsiveViewModel : AbstractViewModel
+	{
+		[NotifyProperty( Binding.Properties.UnresponsiveWindows )]
+		public ObservableCollection<string> UnresponsiveWindows { get; set; }
+
+		[NotifyProperty( Binding.Properties.Desktop )]
+		public VirtualDesktop Desktop { get; private set; }
+
+		[NotifyProperty( Binding.Properties.SelectedApplication )]
+		public int SelectedApplicationIndex { get; set; }
+
+		readonly Dictionary<string, WindowSnapshot> _unresponsiveWindows;
+
+		public List<string> SelectedItems { get; set; }
+
+		public UnresponsiveViewModel( List<WindowSnapshot> windows, VirtualDesktop desktop )
+		{
+			SelectedItems = new List<string>();
+			_unresponsiveWindows = new Dictionary<string, WindowSnapshot>();
+			UnresponsiveWindows = new ObservableCollection<string>();
+
+
+			windows.ForEach( w =>
+			{
+				var processName = w.Window.GetProcess().ProcessName;
+				var processId = w.Window.GetProcess().Id;
+				if ( !_unresponsiveWindows.ContainsKey( processName ) )
+				{
+					UnresponsiveWindows.Add( processName + " (process id: " + processId + ")" );
+					_unresponsiveWindows.Add( processName + " (process id: " + processId + ")", w );
+				}
+			} );
+			Desktop = desktop;
+
+			SetSelection();
+		}
+
+		void RemoveSelected()
+		{
+			// Local copy of selectedItems has to be create because filed is updated both from interface and code.
+			var selectedItemsLocal = new List<string>( SelectedItems );
+
+			selectedItemsLocal.ForEach( myUnresponsive1 => UnresponsiveWindows.Remove( myUnresponsive1 ) );
+		}
+
+		[CommandExecute( Commands.IgnoreSingle )]
+		public void IgnoreSingle()
+		{
+			_unresponsiveWindows.ForEach( window => SelectedItems.ForEach( unresponsive =>
+			{
+				if ( window.Key == unresponsive )
+				{
+					window.Value.Ignore = true;
+				}
+			} ) );
+			
+			RemoveSelected();
+			SetSelection();
+		}
+
+		[CommandExecute( Commands.KeepSingle )]
+		public void KeepSingle()
+		{
+			RemoveSelected();
+			SetSelection();
+		}
+
+		void SetSelection()
+		{
+			if ( UnresponsiveWindows.Count > 0 )
+			{
+				SelectedApplicationIndex = UnresponsiveWindows.Count - 1;
+			}
+		}
+
+		protected override void FreeUnmanagedResources()
+		{
+			// Nothing to do.
+		}
+
+		public override void Persist()
+		{
+			// Nothing to do.
+		}
+	}
+}
