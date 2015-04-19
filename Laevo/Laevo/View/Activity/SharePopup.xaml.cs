9ada14ee5e3a29a11909d261584b454ada5f8312
Steven Jeuris <steven.jeuris@gmail.com>
2015-04-19 19:06:59 +0200

    Refactored popup window: `LaevoPopup` is now a root element and style is no longer separated from template.

---
+++ Laevo/Laevo/View/Activity/SharePopup.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows;
+using Whathecode.System.Windows.DependencyPropertyFactory.Aspects;
+
+
+namespace Laevo.View.Activity
+{
+	/// <summary>
+	///   Interaction logic for SharePopup.xaml
+	/// </summary>
+	[WpfControl( typeof( Properties ) )]
+	public partial class SharePopup
+	{
+		[Flags]
+		public enum Properties
+		{
+		}
+
+
+		public SharePopup()
+		{
+			InitializeComponent();
+		}
+
+		void OnCloseButtonClicked( object sender, RoutedEventArgs e )
+		{
+			Close();
+		}
+	}
+}
--- Laevo/Laevo/View/Common/LaevoPopup.cs
+++ Laevo/Laevo/View/Common/LaevoPopup.cs
@@ -1,19 +1,29 @@
 ﻿using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Media;
 
 
 namespace Laevo.View.Common
 {
-	class LaevoPopup : ContentControl
+	/// <summary>
+	/// Interaction logic for LaevoPopup2.xaml
+	/// </summary>
+	public class LaevoPopup : Window
 	{
 		public static readonly DependencyProperty PopupImageProperty = DependencyProperty.Register(
 			"PopupImage", typeof( ImageSource ),
 			typeof( LaevoPopup ) );
+
 		public ImageSource PopupImage
 		{
 			get { return GetValue( PopupImageProperty ) as ImageSource; }
 			set { SetValue( PopupImageProperty, value ); }
+		}
+
+
+		public LaevoPopup()
+		{
+			WindowStartupLocation = WindowStartupLocation.CenterScreen;
+			Style = Application.Current.FindResource( typeof( LaevoPopup ) ) as Style;
 		}
 	}
 }
