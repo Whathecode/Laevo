901c4af5379c1871cfeb8419ac45dd900c799c04
Dominik Grondziowski <dominik.grondziowski@gmail.com>
2015-09-16 15:15:54 +0200

    Added notification pop-up.

---
+++ Laevo/Laevo/View/Notification/NotificationButtons.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+
+namespace Laevo.View.Notification
+{
+	/// <summary>
+	/// Interaction logic for NotificationButtons.xaml
+	/// </summary>
+	public partial class NotificationButtons
+	{
+		public NotificationButtons()
+		{
+			InitializeComponent();
+		}
+
+		void SetFocus( object sender, MouseButtonEventArgs e )
+		{
+			( (UIElement)e.Source ).Focus();
+		}
+	}
+}
