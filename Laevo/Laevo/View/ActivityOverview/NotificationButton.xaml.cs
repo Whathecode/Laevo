2ca9d6d833e886175104042ac3b9443d447c9fc6
Dominik Grondziowski <5485317+dominkomin@users.noreply.github.com>
2015-09-21 13:25:23 +0200

    Added aggregated view on the available notifications (notification list pop-up and notification button).

---
+++ Laevo/Laevo/View/ActivityOverview/NotificationButton.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Laevo.View.ActivityOverview
+{
+	/// <summary>
+	/// Interaction logic for NotificationButton.xaml
+	/// </summary>
+	public partial class NotificationButton
+	{
+		public NotificationButton()
+		{
+			InitializeComponent();
+		}
+	}
+}
